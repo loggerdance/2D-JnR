d892650fb5fe7b82e3119937b8f1348863d93f92
loggerdance <xdaniel2x@web.de>
2018-02-17 17:10:39 +0100

    Initial commit.

---
+++ Elements/Physical/Entity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Elements.Physical
+{
+	/// <summary>
+	/// Everything that you can see, touch or hear*.
+	/// </summary>
+	public abstract class Entity
+	{
+		public Entity()
+		{
+			
+		}
+		public double X
+		{
+			get; set;
+		}
+		public double Y
+		{
+			get; set;
+		}
+		public double Height
+		{
+			get; set;
+		}
+		public double Width
+		{
+			get; set;
+		}
+	}
+}
